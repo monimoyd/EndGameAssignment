40eecee0bb2d320e3d8e74dbb68c84da2d39d5c4
Monimoy Deb Purkayastha <monimoyd@gmail.com>
2020-04-29 01:20:44 +0530

    TD3 Test Python file

--- TD3_test.py
+++ TD3_test.py
@@ -8,14 +8,23 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 from collections import deque
-import KivyCarEnvironment
+import SimulatedGymEnvironmentFromKivyCar
+
+torch.random.manual_seed(5)
+np.random.seed(5)
 
 if os.path.exists("results") == False:
     os.makedirs("results")
-train_epoch_reward_file= open("results/train_epoch_reward.csv","w")
-eval_epoch_reward_file= open("results/train_epoch_reward.csv","w")
-train_epoch_log= open("results/train_epoch_log.txt","w")
-eval_epoch_log= open("results/eval_epoch_log.txt","w")
+	
+train_epoch_reward_file= open("results/train_epoch_reward.csv","a")
+eval_epoch_reward_file= open("results/eval_epoch_reward.csv","a")
+full_eval_epoch_reward_file=open("results/full_eval_epoch_reward.csv","a")
+train_epoch_log= open("results/train_epoch_log.txt","a")
+eval_epoch_log= open("results/eval_epoch_log.txt","a")
+full_eval_epoch_log= open("results/full_eval_epoch_log.txt","a")
+norm_file=open("results/norm_file.txt","a")
+policy_action_file= open("results/policy_action_file.txt","a")
+
 class ReplayBuffer(object):
 
   def __init__(self, max_size=1e6):
@@ -29,21 +38,23 @@ class ReplayBuffer(object):
       self.ptr = (self.ptr + 1) % self.max_size
     else:
       self.storage.append(transition)
-
-  def sample(self, batch_size):
+	  
+  def sample_base_indexes(self, no_of_samples_from_episode, batch_size, step_size=2):
     #ind = np.random.randint(0, len(self.storage), size=batch_size)
     no_of_records = len(self.storage)
     no_of_episode_records = int(no_of_records/2500)
-    episode_no = random.randint(0, no_of_episode_records-1)
-    offset = random.randint(0, 2500-2*batch_size - 1)
-    start_index = episode_no * 2500 + offset
-    end_index = episode_no * 2500 + offset + 2* batch_size
-    ind = [i for i in range(start_index, end_index, 2)]
+    episode_no_list = np.random.randint(0, no_of_episode_records,size=batch_size )
+    offset_list = np.random.randint(0, 2500-step_size*no_of_samples_from_episode, size=batch_size )
+    index_list = episode_no_list * 2500 + offset_list
+    return index_list
 	
+  
+  #def sample(self, batch_size):
+  def sample(self, index_list):
 	
     (batch_states, batch_states_extra),  (batch_next_states, batch_next_states_extra),  batch_actions, batch_rewards, batch_dones = ([], []),([], []), [], [], []
-    for i in ind: 
-      (state, state_extra), (next_state, next_state_extra), action, reward, done = self.storage[i]
+    for i in  index_list: 
+      (state, state_extra), (next_state, next_state_extra), action, reward, done = self.storage[int(i)]
       batch_states.append(np.array(state, copy=False))
       batch_states_extra.append(np.array(state_extra, copy=False))
       batch_next_states.append(np.array(next_state, copy=False))
@@ -60,29 +71,48 @@ class Actor(nn.Module):
         self.action_dim = action_dim
         self.max_action = max_action
 		
-        self.conv1 = nn.Conv2d(1, 32, 3, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-        self.conv3 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-        self.conv4 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-        self.conv5 = nn.Conv2d(32, 32, 3, stride=1, padding=1)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 32, 3, padding=1, stride=2),
+            nn.BatchNorm2d(32),
+        ) 
+		
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1, stride=2),
+            nn.BatchNorm2d(32),
+        ) 
+		
+        self.conv3= nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1, stride=2),
+            nn.BatchNorm2d(32),
+        ) 
+		
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1, stride=2),
+            nn.BatchNorm2d(32)
+        ) 
+		
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1, stride=1),
+        ) 
+		
+		
 
-        self.lstm = nn.LSTMCell(32, 256)
+        self.lstm = nn.LSTMCell(32, 64)
 
         num_outputs = action_dim
    
-        self.linear1 = nn.Linear(256, 32)
+        self.linear1 = nn.Linear(64, 32)
         self.linear2 = nn.Linear(36, 64)
         self.linear3 = nn.Linear(64, num_outputs)
-        #self.train()
         
     
     def forward(self, inputs):
         (inputs, inputs_extra), (hx, cx) = inputs
         #print("forward: Inputs dim", inputs.shape)
-        x = F.elu(self.conv1(inputs))
-        x = F.elu(self.conv2(x))
-        x = F.elu(self.conv3(x))
-        x = F.elu(self.conv4(x))
+        x = F.relu(self.conv1(inputs))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
         x = self.conv5(x)
         x = F.adaptive_avg_pool2d(x,1)
         x = x.view(-1, 32 * 1 * 1)
@@ -93,11 +123,11 @@ class Actor(nn.Module):
         hx, cx = self.lstm(x, (hx, cx))
         x = hx
 		
-        x = F.tanh(self.linear1(x))
+        x = F.relu(self.linear1(x))
         #print("forward: x dim", x.shape)
         #print("forward: inputs_extra", inputs_extra.shape)
         x_ex = torch.cat([x, inputs_extra], 1)
-        x = F.tanh(self.linear2(x_ex))
+        x = F.relu(self.linear2(x_ex))
         x = self.linear3(x)
 		
         output = self.max_action * torch.tanh(x)
@@ -111,25 +141,63 @@ class Critic(nn.Module):
 
     def __init__(self, state_dim, action_dim):
         super(Critic, self).__init__()
-        self.conv1_1 = nn.Conv2d(1, 32, 3, stride=2, padding=1)
-        self.conv2_1 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-        self.conv3_1 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-        self.conv4_1 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-        self.conv5_1 = nn.Conv2d(32, 32, 3, stride=1, padding=1)
-        self.conv5_1 = nn.Conv2d(32, 32, 3, stride=1, padding=1)
-        self.lstm_1 = nn.LSTMCell(32, 256)  
-        self.linear1_1 = nn.Linear(256, 32)
+		
+        self.conv1_1 = nn.Sequential(
+            nn.Conv2d(1, 32, 3, padding=1, stride=2),
+            nn.BatchNorm2d(32),
+        ) 
+		
+        self.conv2_1 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1, stride=2),
+            nn.BatchNorm2d(32),
+        ) 
+		
+        self.conv3_1 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1, stride=2),
+            nn.BatchNorm2d(32),
+        ) 
+		
+        self.conv4_1 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1, stride=2),
+            nn.BatchNorm2d(32)
+        ) 
+		
+        self.conv5_1 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1, stride=1),
+        ) 
+		
+        
+        self.lstm_1 = nn.LSTMCell(32, 64)  
+        self.linear1_1 = nn.Linear(64, 32)
         self.linear2_1 = nn.Linear(38, 128)
         self.linear3_1 = nn.Linear(128, 1)
 		
-        self.conv1_2 = nn.Conv2d(1, 32, 3, stride=1, padding=1)
-        self.conv2_2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-        self.conv3_2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-        self.conv4_2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-        self.conv5_2 = nn.Conv2d(32, 32, 3, stride=1, padding=1)
+        self.conv1_2 = nn.Sequential(
+            nn.Conv2d(1, 32, 3, padding=1, stride=2),
+            nn.BatchNorm2d(32),
+        ) 
+		
+        self.conv2_2 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1, stride=2),
+            nn.BatchNorm2d(32),
+        ) 
+		
+        self.conv3_2 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1, stride=2),
+            nn.BatchNorm2d(32),
+        ) 
+		
+        self.conv4_2 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1, stride=2),
+            nn.BatchNorm2d(32)
+        ) 
+		
+        self.conv5_2 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1, stride=1),
+        ) 
 
-        self.lstm_2 = nn.LSTMCell(32, 256)  
-        self.linear1_2 = nn.Linear(256, 32)
+        self.lstm_2 = nn.LSTMCell(32, 64)  
+        self.linear1_2 = nn.Linear(64, 32)
         self.linear2_2 = nn.Linear(38, 64)
         self.linear3_2 = nn.Linear(64, 1)
         #self.train()
@@ -139,26 +207,26 @@ class Critic(nn.Module):
         (inp, inp_extra), (hx1, cx1), (hx2, cx2) = inputs
 		
         input1 = inp
-        x1 = F.elu(self.conv1_1(input1))
-        x1 = F.elu(self.conv2_1(x1))
-        x1 = F.elu(self.conv3_1(x1))
-        x1 = F.elu(self.conv4_1(x1))
+        x1 = F.relu(self.conv1_1(input1))
+        x1 = F.relu(self.conv2_1(x1))
+        x1 = F.relu(self.conv3_1(x1))
+        x1 = F.relu(self.conv4_1(x1))
         x1 = self.conv5_1(x1)
         x1 = F.adaptive_avg_pool2d(x1,1)
         x1 = x1.view(-1, 32 * 1 * 1)
         hx1, cx1 = self.lstm_1(x1, (hx1, cx1))
         x1 = hx1
-        x1 = F.tanh(self.linear1_1(x1)) 	
+        x1 = F.relu(self.linear1_1(x1)) 	
         x1_ex1_u1 = torch.cat([x1,  inp_extra, u], 1)
-        x1 = F.tanh(self.linear2_1(x1_ex1_u1))
+        x1 = F.relu(self.linear2_1(x1_ex1_u1))
         x1 = self.linear3_1(x1)
 		
 		
         input2 = inp
-        x2 = F.elu(self.conv1_2(input2))
-        x2 = F.elu(self.conv2_2(x2))
-        x2 = F.elu(self.conv3_2(x2))
-        x2 = F.elu(self.conv4_2(x2))
+        x2 = F.relu(self.conv1_2(input2))
+        x2 = F.relu(self.conv2_2(x2))
+        x2 = F.relu(self.conv3_2(x2))
+        x2 = F.relu(self.conv4_2(x2))
         x2 = self.conv5_2(x2)
 
         x2 = F.adaptive_avg_pool2d(x2,1)
@@ -166,9 +234,9 @@ class Critic(nn.Module):
 		
         hx2, cx2 = self.lstm_2(x2, (hx2, cx2))
         x2 = hx2
-        x2 = F.tanh(self.linear1_2(x2)) 	
+        x2 = F.relu(self.linear1_2(x2)) 	
         x2_ex2_u2 = torch.cat([x2,  inp_extra, u], 1)
-        x2 = F.tanh(self.linear2_2(x2_ex2_u2))
+        x2 = F.relu(self.linear2_2(x2_ex2_u2))
         x2 = self.linear3_2(x2)        
 		
         return x1, x2
@@ -177,18 +245,18 @@ class Critic(nn.Module):
         (inp, inp_extra), (hx1, cx1) = inputs
 		
         input1 = inp
-        x1 = F.elu(self.conv1_1(input1))
-        x1 = F.elu(self.conv2_1(x1))
-        x1 = F.elu(self.conv3_1(x1))
-        x1 = F.elu(self.conv4_1(x1))
+        x1 = F.relu(self.conv1_1(input1))
+        x1 = F.relu(self.conv2_1(x1))
+        x1 = F.relu(self.conv3_1(x1))
+        x1 = F.relu(self.conv4_1(x1))
         x1 = self.conv5_1(x1)
         x1 = F.adaptive_avg_pool2d(x1,1)
         x1 = x1.view(-1, 32 * 1 * 1)
         hx1, cx1 = self.lstm_1(x1, (hx1, cx1))
         x1 = hx1
-        x1 = F.tanh(self.linear1_1(x1)) 	
+        x1 = F.relu(self.linear1_1(x1)) 	
         x1_ex1_u1 = torch.cat([x1,  inp_extra, u], 1)
-        x1 = F.tanh(self.linear2_1(x1_ex1_u1))
+        x1 = F.relu(self.linear2_1(x1_ex1_u1))
         x1 = self.linear3_1(x1)
 		
         return x1
@@ -208,11 +276,13 @@ class TD3(object):
     self.actor = Actor(state_dim, action_dim, max_action).to(device)
     self.actor_target = Actor(state_dim, action_dim, max_action).to(device)
     self.actor_target.load_state_dict(self.actor.state_dict())
-    self.actor_optimizer = torch.optim.Adam(self.actor.parameters())
+    #self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=1e-4)
+    self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=1e-5,  weight_decay=0.1, amsgrad=True)
     self.critic = Critic(state_dim, action_dim).to(device)
     self.critic_target = Critic(state_dim, action_dim).to(device)
     self.critic_target.load_state_dict(self.critic.state_dict())
-    self.critic_optimizer = torch.optim.Adam(self.critic.parameters())
+    #self.critic_optimizer = torch.optim.Adam(self.critic.parameters() , lr=2e-4)
+    self.critic_optimizer = torch.optim.Adam(self.critic.parameters() , lr=1e-5,  weight_decay=0.1, amsgrad=True)
     self.max_action = max_action
 
   def select_action(self, state):
@@ -227,24 +297,31 @@ class TD3(object):
 
   def train(self, replay_buffer, iterations, batch_size=128, discount=0.99, tau=0.005, policy_noise=0.2, noise_clip=0.5, policy_freq=2):
 	  
-    actor_hx = torch.zeros(128, 256).to(device)
-    actor_cx = torch.zeros(128, 256).to(device)
-    actor_target_hx = torch.zeros(128, 256).to(device)
-    actor_target_cx = torch.zeros(128, 256).to(device)
-    critic1_hx = torch.zeros(128, 256).to(device)
-    critic1_cx = torch.zeros(128, 256).to(device)
-    critic1_target_hx = torch.zeros(128, 256).to(device)
-    critic1_target_cx = torch.zeros(128, 256).to(device)
-    critic2_hx = torch.zeros(128, 256).to(device)
-    critic2_cx = torch.zeros(128, 256).to(device)
-    critic2_target_hx = torch.zeros(128, 256).to(device)
-    critic2_target_cx = torch.zeros(128, 256).to(device)  	
+    actor_hx = torch.zeros(128, 64).to(device)
+    actor_cx = torch.zeros(128, 64).to(device)
+    actor_target_hx = torch.zeros(128, 64).to(device)
+    actor_target_cx = torch.zeros(128, 64).to(device)
+    critic1_hx = torch.zeros(128, 64).to(device)
+    critic1_cx = torch.zeros(128, 64).to(device)
+    critic1_target_hx = torch.zeros(128, 64).to(device)
+    critic1_target_cx = torch.zeros(128, 64).to(device)
+    critic2_hx = torch.zeros(128, 64).to(device)
+    critic2_cx = torch.zeros(128, 64).to(device)
+    critic2_target_hx = torch.zeros(128, 64).to(device)
+    critic2_target_cx = torch.zeros(128, 64).to(device)  
+    no_of_samples_from_episode = 128
+    step_size = 2	
 
-    
+    index_numpy_base_list = replay_buffer.sample_base_indexes(no_of_samples_from_episode, batch_size, step_size=step_size)
+    #print("index_numpy_base_list = " , index_numpy_base_list)
+    ones_batch = np.ones(batch_size)
     for it in range(iterations):
-      
+	
+      index_list = (index_numpy_base_list + step_size * it* ones_batch).tolist()
+      #print("index_list ", index_list)
       # Step 4: We sample a batch of transitions (s, s’, a, r) from the memory
-      batch_states_full, batch_next_states_full, batch_actions, batch_rewards, batch_dones = replay_buffer.sample(batch_size)
+      #batch_states_full, batch_next_states_full, batch_actions, batch_rewards, batch_dones = replay_buffer.sample(batch_size)
+      batch_states_full, batch_next_states_full, batch_actions, batch_rewards, batch_dones = replay_buffer.sample(index_list)
       #print("batch_states_full: ", batch_states_full)
       #print("batch_next_states_full: ", batch_next_states_full)
       batch_states, batch_states_extra = batch_next_states_full
@@ -331,19 +408,20 @@ class TD3(object):
     self.actor.load_state_dict(torch.load('%s/%s_actor.pth' % (directory, filename)))
     self.critic.load_state_dict(torch.load('%s/%s_critic.pth' % (directory, filename)))
 
-def evaluate_policy(env, policy, train_epoch_num=0, eval_episodes=2):
+def evaluate_policy(env, policy, train_episode_num=0, eval_episodes=10, mode="Eval"):
   global eval_epoch_reward_file
   global eval_epoch_log
+  global full_eval_epoch_reward_file
 
   avg_reward = 0.
   
-  for _ in range(eval_episodes):
-    obs = env.reset("Eval")
+  for eval_episode_num in range(eval_episodes):
+    obs = env.reset(mode, train_episode_num, eval_episode_num + 1)
     #print("evaluate_policy: obs: ", obs)
     #print("evaluate_policy: typr: ", type(obs))
     done = False
-    hx_actor = torch.zeros ((1,256)).to(device)
-    cx_actor = torch.zeros ((1,256)).to(device)
+    hx_actor = torch.zeros ((1,64)).to(device)
+    cx_actor = torch.zeros ((1,64)).to(device)
     while not done:
       #action = policy.select_action(np.array(obs))
 
@@ -354,12 +432,24 @@ def evaluate_policy(env, policy, train_epoch_num=0, eval_episodes=2):
       avg_reward += reward
   avg_reward /= eval_episodes
   print ("---------------------------------------\n")
-  print ("Average Reward over the Evaluation Step: %f" % (avg_reward))
+  if train_episode_num>0 :
+      print ("Train Episode Num: %d,  Average Reward over the Evaluation Step: %f" % ( train_episode_num, avg_reward))
+  else:
+      print ("Average Reward over the Evaluation Steps: %f" % ( avg_reward))
   print ("---------------------------------------\n")
-  eval_epoch_log.write("---------------------------------------")
-  eval_epoch_log.write(" After train episode: %d, Average Reward over the Evaluation Step: %f" % (train_epoch_num, avg_reward))
-  eval_epoch_log.write("---------------------------------------")
-  eval_epoch_reward_file.write(str(train_epoch_num) + "," + str(avg_reward) + "\n")
+  eval_epoch_log.write("---------------------------------------\n")
+  if train_episode_num>0 :
+      eval_epoch_log.write(" After train episode: %d, eval episode: %d,  Average Reward over the Evaluation Step: %f \n" % (train_episode_num, eval_episode_num, avg_reward))
+  else:
+      full_eval_epoch_log.write(" Average Reward over the Evaluation Step: %f \n" % (avg_reward))
+  
+  eval_epoch_log.write("---------------------------------------\n")
+  if train_episode_num>0 :
+      eval_epoch_reward_file.write(str(train_episode_num) + "," + str(avg_reward) + "\n")
+      eval_epoch_reward_file.flush()
+  else:
+      full_eval_epoch_reward_file.write(str(avg_reward) + "\n")
+      full_eval_epoch_reward_file.flush()
   return avg_reward
   
 def mkdir(base, name):
@@ -368,17 +458,24 @@ def mkdir(base, name):
         os.makedirs(path)
     return path
 
-
 if __name__ == '__main__':
     env_name = "kivy-car"
-    seed = 0 # Random seed number
-    file_name = "%s_%s_%s" % ("TD3", env_name, str(seed))
+    seed = 5 # Random seed number
+    file_name = "%s_%s_%s" % ("TD3_best", env_name, str(seed))
+    #file_name = "%s_%s_%s_%s" % ("TD3", env_name, str(seed), str(72))
     print ("---------------------------------------")
     print ("Settings: %s" % (file_name))
     print ("---------------------------------------")
+	# Need to change destination in map
+	# 144  Aversge reward: -2737
+    # Best  Aversge reward: -2737
+	# 192 Average Reward: - 2689 (Hit the goal twice)
+	# 172 Average Reward: -2933
+	# 102 Average Reward: -1825 (Better)
+	# 72 Average Reward: -2613
 
     eval_episodes = 1
-    env = KivyCarEnvironment.KivyCarEnvironment()
+    env = SimulatedGymEnvironmentFromKivyCar.SimulatedGymEnvironmentFromKivyCar()
     env.start()
     max_episode_steps = env.max_episode_steps()
     #if save_env_vid:
@@ -392,7 +489,7 @@ if __name__ == '__main__':
     max_action = 5.0
     policy = TD3(state_dim, action_dim, max_action)
     policy.load(file_name, './pytorch_models/')
-    _ = evaluate_policy(env, policy, eval_episodes=50)
+    _ = evaluate_policy(env, policy, eval_episodes=2,  mode="Full_Eval")
 		
 
 
